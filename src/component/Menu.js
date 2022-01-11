5f110cd22952b56ab59b3ee9903700e2e5a949c6
T.Praksit <prakasit.tar@mail.kmutt.ac.th>
2022-01-11 13:43:08 +0700

    update

---
+++ src/component/Menu.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import {StyleSheet, View, Text} from 'react-native'
+
+export default function Menu() {
+    return {
+        <View style={styles.container}>
+            <Text> Menu Appication </Text>
+            <StatusBar style="auto">
+        </View>
+    }
+}
